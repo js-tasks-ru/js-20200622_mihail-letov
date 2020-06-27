ea53c8347b2441be309687cec93eda16d0405c57
Mikhail.Letov <mletov@yandex.ru>
2020-06-27 20:04:06 +0300

    Solve 1-sort-strings (part 2)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,26 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
+ 
+//https://thread.engineering/2018-08-29-searching-and-sorting-text-with-diacritical-marks-in-javascript/
+ 
+export function sortStrings(arr, param = 'asc') {
+	var arRes = arr.slice().sort(compareFunction);
+	if (param === 'desc') arRes = arRes.reverse();
+	return arRes;
+}
 
+function compareFunction(a, b) {
+
+	if (a === b) return 0;
+
+	var aWithoutDiacritics = a.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	var bWithoutDiacritics = b.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+		
+	if (aWithoutDiacritics.toLowerCase() > bWithoutDiacritics.toLowerCase()) return 1;
+	if (aWithoutDiacritics.toLowerCase() < bWithoutDiacritics.toLowerCase()) return -1;
+
+    if (a > b) return 1;
+    if (a < b) return -1;	
+	
 }
