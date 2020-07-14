419b4500bd829074d674a2875184f6503071a7bc
unknown <mletov@yandex.ru>
2020-07-14 16:32:36 +0300

    pointer

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,32 @@
 class Tooltip {
+    initialize() {
+        
+        const pointerAreas = document.querySelectorAll(`[data-tooltip]:not([data-tooltip=""])`);
+        pointerAreas.forEach(area => {
 
+            area.addEventListener('pointerover', (event) => {
+                console.log('Pointer moved in');
+            }); 
+
+            area.addEventListener('pointerout', (event) => {
+                console.log('Pointer moved in');
+            }); 
+
+        });
+
+       
+    }
+
+    create() {
+        
+    }
+
+    destroy() {
+
+    }
 }
 
 const tooltip = new Tooltip();
+Object.freeze(tooltip);
 
 export default tooltip;
