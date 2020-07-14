3c2f40f38f77f8819f525f61c455c2a8c0c84195
unknown <mletov@yandex.ru>
2020-07-14 17:45:12 +0300

    cursor

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,28 +1,41 @@
 class Tooltip {
+
+    constructor() {
+        this.element = this.create();
+    }
+
     initialize() {
         
         const pointerAreas = document.querySelectorAll(`[data-tooltip]:not([data-tooltip=""])`);
         pointerAreas.forEach(area => {
 
             area.addEventListener('pointerover', (event) => {
-                console.log('Pointer moved in');
+                document.body.append(this.element);
             }); 
 
             area.addEventListener('pointerout', (event) => {
-                console.log('Pointer moved in');
+                this.destroy();
             }); 
 
-        });
+            area.addEventListener('mousemove', (event) => {
+                this.element.style.left = event.screenX + 'px';
+                this.element.style.top = event.screenY + 'px';
+            });
 
-       
+        });
+    
     }
 
     create() {
-        
+        const tooltip = document.createElement("div");
+        tooltip.setAttribute("classs", "tooltip");
+        return tooltip;
     }
 
     destroy() {
-
+        if (this.element) {
+            this.element.remove();
+        }
     }
 }
 
