51f69acd110a270c2506946f5b5af4d3ca1f84db
Mikhail.Letov <mletov@yandex.ru>
2020-07-16 22:01:42 +0300

    TEST

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,26 @@
 export default class DoubleSlider {
 
+    constructor(options = {}) {
+        this.min = options.min || 100;
+        this.max = options.max || 200;
+        this.formatValue = options.formatValue || (value => { return value });
+
+        this.element = this.createElement();
+
+        window.addEventListener("mousedown", null, options);
+    }
+
+    createElement() {
+        const wrapper = document.createElement("div")
+        wrapper.setAttribute("class", "range-slider");
+        wrapper.innerHTML = `<span>${this.formatValue(this.min)}</span>
+            <div class="range-slider__inner">
+                <span class="range-slider__progress"></span>
+                <span class="range-slider__thumb-left"></span>
+                <span class="range-slider__thumb-right"></span>
+            </div>
+            <span>${this.formatValue(this.max)}</span>`;
+        return wrapper;
+    }
+
 }
